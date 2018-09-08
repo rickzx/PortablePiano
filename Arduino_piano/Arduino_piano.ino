bb299dd09978c02fe16f2d501fc480a3f10187ea
zxdefg <zxdefg@126.com>
2018-09-07 21:44:38 -0400

    Added hardware files

---
+++ Arduino_piano/Arduino_piano.ino
@@ -0,0 +1,388 @@
+#include <SD.h>
+
+#include <CapacitiveSensor.h>
+
+#define total 10                   //define sensitivity, high value for decreases sensitivity, low value increases
+#define sensor 1                  //define number of samples Arduino takes, high value will increase stability while increasing response time
+#define replay 35                 //define debouncing, high value will increase stability while increasing response time
+int led = 13;                                         
+
+
+int state = HIGH;
+
+boolean yes12;
+boolean previous12 = false;
+
+boolean yes11;
+boolean previous11 = false;
+
+boolean yes10;
+boolean previous10 = false;
+
+boolean yes9;
+boolean previous9 = false;
+
+boolean yes8;
+boolean previous8 = false;
+
+boolean yes7;
+boolean previous7 = false;
+
+boolean yes6;
+boolean previous6 = false;
+
+boolean yes5;
+boolean previous5 = false;
+
+boolean yes4;
+boolean previous4 = false;
+
+boolean yes3;
+boolean previous3 = false;
+
+
+CapacitiveSensor   cs_2_12 = CapacitiveSensor(2,12);        // 2.2M resistor between pins 2 & 12, pin 2 is send pin, pin 12 is sensor pin
+CapacitiveSensor   cs_2_11 = CapacitiveSensor(2,11);        // 2.2M resistor between pins 2 & 11, pin 2 is send pin, pin 11 is sensor pin
+CapacitiveSensor   cs_2_10 = CapacitiveSensor(2,10);        // 2.2M resistor between pins 2 & 10, pin 2 is send pin, pin 10 is sensor pin
+CapacitiveSensor   cs_2_9 = CapacitiveSensor(2,9);        
+CapacitiveSensor   cs_2_8 = CapacitiveSensor(2,8);        
+CapacitiveSensor   cs_2_7 = CapacitiveSensor(2,7);       
+CapacitiveSensor   cs_2_6 = CapacitiveSensor(2,6);      
+CapacitiveSensor   cs_2_5 = CapacitiveSensor(2,5);        
+CapacitiveSensor   cs_2_4 = CapacitiveSensor(2,4);      
+CapacitiveSensor   cs_2_3 = CapacitiveSensor(2,3);       
+
+
+void setup()                    
+{
+   cs_2_12.set_CS_AutocaL_Millis(0xFFFFFFFF);  //Calibrate the sensor... 
+   cs_2_11.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_10.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_9.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_8.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_7.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_6.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_5.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_4.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_2_3.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   pinMode(led, OUTPUT);
+   Serial.begin(115200);
+}
+
+short int a=1,a1=0,b=1,b1=0,c=1,c1=0,d=1,d1=0,e=1,e1=0,f=1,f1=0,g=1,g1=0,h=1,h1=0,i=1,i1=0,j=1,j1=0;  //for debounce purpose
+
+
+void loop()                    
+{    
+  
+  
+    long total112 =  cs_2_12.capacitiveSensor(sensor);
+    long total111 =  cs_2_11.capacitiveSensor(sensor);
+    long total110 =  cs_2_10.capacitiveSensor(sensor);
+    long total19 =  cs_2_9.capacitiveSensor(sensor);
+    long total18 =  cs_2_8.capacitiveSensor(sensor);
+    long total17 =  cs_2_7.capacitiveSensor(sensor);
+    long total16 =  cs_2_6.capacitiveSensor(sensor);
+    long total15 =  cs_2_5.capacitiveSensor(sensor);
+    long total14 =  cs_2_4.capacitiveSensor(sensor);
+    long total13 =  cs_2_3.capacitiveSensor(sensor);
+    
+    if (total112 > total){yes12 = true;}
+    else {yes12 = false;} 
+ 
+    if (total111 > total){yes11 = true;}
+    else {yes11 = false;}
+
+    if (total110 > total){yes10 = true;}
+    else {yes10 = false;} 
+ 
+    if (total19 > total){yes9 = true;}
+    else {yes9 = false;}
+
+    if (total18 > total){yes8 = true;}
+    else {yes8 = false;}
+   
+    if (total17 > total){yes7 = true;}
+    else {yes7 = false;} 
+   
+    if (total16 > total){yes6 = true;}
+    else {yes6 = false;}
+  
+    if (total15 > total){yes5 = true;}
+    else {yes5 = false;} 
+   
+    if (total14 > total){yes4 = true;}
+    else {yes4 = false;}
+   
+    if (total13 > total){yes3 = true;}
+    else {yes3 = false;} 
+     
+    
+    
+    if(yes12 == true && previous12  == false && a)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+          }
+       else 
+         state = LOW;
+         a = 0;
+         a1 = 0;
+        Serial.println('a');     
+       
+    }
+    if(yes12 == false && previous12  == false)
+    a1++;
+    else
+    a1=0;
+    if(a1==replay)
+    {
+    a=1;
+    a1=0;
+    }
+        
+    
+    if(yes11 == true && previous11  == false && b)
+
+    {
+  
+       if(state == LOW){
+         state = HIGH;
+         }
+       else 
+         state = LOW;
+         b = 0;
+         b1 = 0;
+        Serial.println('b');     
+    
+     }
+    if(yes11 == false && previous11  == false)
+    b1++;
+    else
+    b1=0;
+    if(b1==replay)
+    {
+    b=1;
+    b1=0;
+    }
+    
+   
+    if(yes10 == true && previous10  == false && c)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+        }
+       else 
+         state = LOW;
+         c = 0;
+         c1 = 0;
+        Serial.println('c');     
+       
+    }
+    if(yes10== false && previous10  == false)
+    c1++;
+    else
+    c1=0;
+    if(c1==replay)
+    {
+    c=1;
+    c1=0;
+    }
+   
+  
+    if(yes9 == true && previous9  == false && d)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+       }
+       else 
+         state = LOW;
+         d = 0;
+         d1 = 0;
+        Serial.println('d');     
+       
+    }
+    if(yes9== false && previous9  == false)
+    d1++;
+    else
+    d1=0;
+    if(d1==replay)
+    {
+    d=1;
+    d1=0;
+    }
+     
+   if(yes8 == true && previous8  == false && e)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+       }
+       else 
+         state = LOW;
+         e = 0;
+         e1 = 0;
+        Serial.println('e');     
+       
+    }
+    if(yes8== false && previous8  == false)
+    e1++;
+    else
+    e1=0;
+    if(e1==replay)
+    {
+    e=1;
+    e1=0;
+    }
+   
+   if(yes7 == true && previous7  == false && f)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+         }
+       else 
+         state = LOW;
+         f = 0;
+         f1 = 0;
+        Serial.println('f');     
+       
+    }
+    if(yes7== false && previous7  == false)
+    f1++;
+    else
+    f1=0;
+    if(f1==replay)
+    {
+    f=1;
+    f1=0;
+    }
+  
+  if(yes6 == true && previous6  == false && g)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+       }
+       else 
+         state = LOW;
+         g = 0;
+         g1 = 0;
+        Serial.println('g');     
+       
+    }
+    if(yes6== false && previous6  == false)
+    g1++;
+    else
+    g1=0;
+    if(g1==replay)
+    {
+    g=1;
+    g1=0;
+    }
+ 
+ 
+  if(yes5 == true && previous5  == false && h)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+       }
+       else 
+         state = LOW;
+         h = 0;
+         h1 = 0;
+        Serial.println('h');     
+       
+    }
+    if(yes5== false && previous5  == false)
+    h1++;
+    else
+    h1=0;
+    if(h1==replay)
+    {
+    h=1;
+    h1=0;
+    }
+
+
+   if(yes4 == true && previous4  == false && i)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+       }
+       else 
+         state = LOW;
+         i = 0;
+         i1 = 0;
+        Serial.println('i');     
+       
+    }
+    if(yes4== false && previous4  == false)
+    i1++;
+    else
+    i1=0;
+    if(i1==replay)
+    {
+    i=1;
+    i1=0;
+    }
+
+
+    if(yes3 == true && previous3  == false && j)
+    
+    {
+      
+       if(state == LOW){
+         state = HIGH;
+       }
+       else 
+         state = LOW;
+         j = 0;
+         j1 = 0;
+        Serial.println('j');     
+       
+    }
+    if(yes3== false && previous3  == false)
+    j1++;
+    else
+    j1=0;
+    if(j1==replay)
+    {
+    j=1;
+    j1=0;
+    }    
+    
+         
+      digitalWrite(led, state);
+      previous12 = yes12;
+      previous11 = yes11;
+      previous10 = yes10;
+      previous9 = yes9;
+      previous8 = yes8;
+      previous7 = yes7;
+      previous6 = yes6;
+      previous5 = yes5;
+      previous4 = yes4;
+      previous3 = yes3;
+           
+     
+  
+
+      
+      delay(2);
+
+
+}
---
+++ Processing_piano/Processing_piano.pde
@@ -0,0 +1,215 @@
+import ddf.minim.*;
+import processing.serial.*;
+
+Minim minim;
+AudioPlayer song40; 
+AudioPlayer song41;
+AudioPlayer song42;
+AudioPlayer song43;
+AudioPlayer song44;
+AudioPlayer song45;
+AudioPlayer song46;
+AudioPlayer song47;
+AudioPlayer song48;
+AudioPlayer song49;
+
+
+Serial myPort;  // Create object from Serial class
+String val;     // Data received from the serial port
+
+int r=0;
+int m=1;  
+void setup()
+{
+ minim = new Minim(this); 
+ song40 = minim.loadFile("40.wav");
+ song41 = minim.loadFile("41.wav");
+ song42 = minim.loadFile("42.wav");
+ song43 = minim.loadFile("43.wav");
+ song44 = minim.loadFile("44.wav");
+ song45 = minim.loadFile("45.wav");
+ song46 = minim.loadFile("46.wav");
+ song47 = minim.loadFile("47.wav");
+ song48 = minim.loadFile("48.wav");
+ song49 = minim.loadFile("49.wav");
+ 
+   
+  
+ String portName = "/dev/cu.usbmodem1411";                             //Change COM48 to the port number that your Arduino is connected, you can check the port number from arduino program. 
+ myPort = new Serial(this, portName, 115200);   
+
+ size(160, 160);
+ background(0);
+  
+  
+}
+
+
+void draw()
+{
+ 
+  if ( myPort.available() > 0 ) 
+      {  // If data is available,
+     
+      val = myPort.readStringUntil('\n');
+ if(val!=null)
+ {print(val);
+ 
+      if(val.charAt(0)=='a')
+      {
+      song40.rewind();
+      song40.play();
+     
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("DO", 80, 90);
+      
+      } 
+      
+      if(val.charAt(0)=='b')
+      {
+      song41.rewind();
+      song41.play(); 
+      
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("DO#", 80, 90);
+     
+      
+      }
+      
+      if(val.charAt(0)=='c')
+      {
+      song42.rewind();
+      song42.play(); 
+      
+      fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("RE", 80, 90);
+      } 
+      
+      if(val.charAt(0)=='d')
+      {
+      song43.rewind();
+      song43.play();
+     
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("RE#", 80, 90); 
+      } 
+      
+      if(val.charAt(0)=='e')
+      {
+      song44.rewind();
+      song44.play();
+     
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("MI", 80, 90); 
+      }
+     
+     if(val.charAt(0)=='f')
+      {
+      song45.rewind();
+      song45.play(); 
+      
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("FA", 80, 90);
+      }
+    
+     if(val.charAt(0)=='g')
+      {
+      song46.rewind();
+      song46.play();
+      
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("FA#", 80, 90); 
+      }
+    
+     if(val.charAt(0)=='h')
+      {
+      song47.rewind();
+      song47.play(); 
+      
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("SOL", 80, 90); 
+      }
+ 
+     if(val.charAt(0)=='i')
+      {
+      song48.rewind();
+      song48.play();
+     
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("SOL#", 80, 90);  
+      } 
+
+     if(val.charAt(0)=='j')
+      {
+      song49.rewind();
+      song49.play();
+     
+     fill(0, 0, 0);
+     rect(0, 0, 160, 160);            
+     fill(255, 255, 255);
+     textAlign(CENTER);      
+     textSize(50);  
+     text("LA", 80, 90); 
+      }        
+      
+         
+                           
+ }         
+       }
+       
+   
+   
+  if (mousePressed == true)
+         {if(r==1)
+          {myPort.write(' ');
+          background(0);}
+          r=0;
+          m=1;}
+   
+   
+ 
+    
+
+        
+}
+
+
+
+public void stop() {
+minim.stop(); }
