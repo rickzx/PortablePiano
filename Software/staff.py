269b626b8c11b8cbfecbf9665db18435d4e289fd
zxdefg <zxdefg@126.com>
2018-09-08 06:50:56 -0400

    Add staff class

---
+++ Software/staff.py
@@ -0,0 +1,17 @@
+import pygame
+from config import *
+
+class Staff(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self,self.groups)
+		self.image = self.loadImg()
+		self.mask = pygame.mask.from_surface(self.image)
+		self.rect = (0, 0, Config.display_width, Config.display_height)
+
+	def loadImg(self):
+		surface = pygame.Surface((Config.display_width, Config.display_height // 3))
+		surface.fill((0,0,0))
+		return surface
+
+	def update(self, seconds):
+		pass
