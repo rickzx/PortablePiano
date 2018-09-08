08f8c4d4cb4afc2bbe4566949f6f603a4251bd91
Shuwen <shuwen@huntsman-ve702-3195.apng.wlan.private.upenn.edu>
2018-09-08 10:41:12 -0400

    metronome

---
+++ Software/metronome.py
@@ -0,0 +1,22 @@
+import pygame, sys, math
+import time
+
+def PlayCounting(bpm, bpb):
+    sleep = 60 / bpm
+    counter = 0
+    metronomeSound = pygame.mixer.Sound("media/metronome_klack.wav")
+    while True:
+        if counter % bpb == 0:
+            metronomeSound.set_volume(1.0)
+            metronomeSound.play()
+        else:
+            metronomeSound.set_volume(0.6)
+            metronomeSound.play()
+        time.sleep(sleep)
+
+def main():
+    pygame.init()
+    PlayCounting(80, 2)
+
+if __name__ == '__main__':
+    main()
